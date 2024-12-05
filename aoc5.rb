b8215c4ac4185f789c45f98dc444831c79c3e34e
Jing Guo <dev.guoj@gmail.com>
2024-12-05 13:30:39 +0100

    faster solution based on sorting

--- aoc5.rb
+++ aoc5.rb
@@ -19,29 +19,45 @@ end
 
 valid_updates = updates.select { |update| valid_update?(update, rules) }
 
-# puts sum_of_middle_numbers(valid_updates)
+puts sum_of_middle_numbers(valid_updates)
 
 # Problem 2
 
-def make_valid_update!(update, rules)
-  changed = true
+# inefficient solution
+#
+# def make_valid_update!(update, rules)
+#   changed = true
+#
+#   while changed
+#     changed = false
+#     (0...update.length - 1).each do |index|
+#       page_pair = [update[index], update[index + 1]]
+#
+#       if rules.include?(page_pair.reverse)
+#         update[index], update[index + 1] = page_pair.reverse
+#         changed = true
+#       end
+#     end
+#   end
+#
+#   update
+# end
 
-  while changed
-    changed = false
-    (0...update.length - 1).each do |index|
-      page_pair = [update[index], update[index + 1]]
+# solution based on sorting
 
-      if rules.include?(page_pair.reverse)
-        update[index], update[index + 1] = page_pair.reverse
-        changed = true
-      end
+def valid_comparator(rules)
+  lambda do |x, y|
+    if rules.include?([x, y]) # x comes before y
+      -1
+    elsif rules.include?([y, x])
+      1
+    else
+      0
     end
   end
-
-  update
 end
 
 invalid_updates = updates - valid_updates
-after_valid_updates = invalid_updates.map { |update| make_valid_update!(update, rules)  }
+after_valid_updates = invalid_updates.map { |update| update.sort(&valid_comparator(rules))  }
 
 puts sum_of_middle_numbers(after_valid_updates)
